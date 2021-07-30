14defd27bcaf7118be07f423b4742971e642bf6f
unknown <sarthakkrishna84@gmail.com>
2021-07-31 01:16:16 +0530

    Deleted the side branch and clear the contents in the .js file

--- first.js
+++ first.js
@@ -1,14 +1,2 @@
 x = 7;
 console.log(x);
-console.log(56);
-console.log(69);
-y = 6;
-console.log(y+6);
-console.log(2);
-console.log(3);
-z = 4;
-console.log(y + z);
-console.log(360);
-console.log("Hi baby");
-console.log(24);
-console.log(7563);
